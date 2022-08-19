7c8fa7bcc5079c41e72dae163c0fafb134918efb
Marcelinhoj <m_seoy@msn.com>
2022-08-18 21:58:34 -0300

    1

---
+++ cursos/11aula/008ex.js
@@ -0,0 +1,6 @@
+var vel = 55
+console.log( `A velocidade do seu carro é ${vel} Km/h`)
+if( vel > 60){
+    console.log(`Você ultrapassou a velocidade permitida. Multado!`)
+}
+console.log(`Dirija sempre com o sinto de segurança!`)
