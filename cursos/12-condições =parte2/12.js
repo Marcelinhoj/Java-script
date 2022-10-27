c385a82ef6c3561092b21a5e1c1c02928ef52bf8
Marcelinhoj <m_seoy@msn.com>
2022-10-27 20:57:19 -0300

    1

---
+++ cursos/12-condições =parte2/12.js
@@ -0,0 +1,12 @@
+var agora = new Date()
+var hora = 8
+console.log (`Agora são exatamente ${hora} horas`)
+if (hora  < 12 && hora >=6 ){
+    console.log('Bom dia!')
+} else if (hora <= 18){
+    console.log('Boa Tarde!!')
+} else if (hora <= 24){
+    console.log('Boa Noite!!')
+}else (hora <=5) ;{
+    console.log('Boa Madrugada!!')
+}
