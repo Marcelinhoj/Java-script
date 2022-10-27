984497b89488f18c2e4ea23ab42dbc20ea5c616b
Marcelinhoj <m_seoy@msn.com>
2022-10-27 20:20:53 -0300

    1

--- cursos/12-condições =parte2/11.js
+++ cursos/12-condições =parte2/11.js
@@ -1,8 +1,11 @@
 var idade =18
-if (idade < 18){
+console.log(`Você tem ${idade} anos`)
+if (idade < 16){
     console.log( 'Não vota')
+}else  if (idade < 18 || idade > 65){
+    console.log('Voto Opcional')
 }else {
-    console.log('Vota')
+    console.log('Voto Obrigatório')
 }
 
     
