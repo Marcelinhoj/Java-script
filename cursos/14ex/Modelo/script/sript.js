33f7729d1d77d5a54dc2ef6b3c035794b2b86f52
Marcelinhoj <m_seoy@msn.com>
2022-10-31 22:29:29 -0300

    1

--- cursos/14ex/Modelo/script/sript.js
+++ cursos/14ex/Modelo/script/sript.js
@@ -1,1 +1,22 @@
-alert('Olá')
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas`
+
+    if ( hora >= 0 && hora < 12){
+        //BOM DIA !
+        img.src = 'fotomanha.png'
+        document.body.style.background = ''
+        
+    } else if ( hora >= 12 && hora <= 18){
+        //BOA TARDE
+        img.src = 'fototarde.png'
+        document.body.style.background = ''
+    } else {
+        //BOA NOITE!
+        img.src = 'fotonoite.png'
+        document.body.style.background = ''
+    }
+}
