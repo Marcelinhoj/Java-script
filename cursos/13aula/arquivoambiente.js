2d6d47a4fe4d3ff25450e743cd5c96639d07f188
Marcelinhoj <m_seoy@msn.com>
2022-11-14 20:31:54 -0300

    1

--- cursos/13aula/arquivoambiente.js
+++ cursos/13aula/arquivoambiente.js
@@ -1,12 +1,17 @@
 var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++    
+} while (c <= 6)
+
+
+/*
+var c = 1
 while ( c <=6){
     console.log('Tudo bem?')
     c++ 
 }
-
-
-
-
+*/
 
 /*
 console.log('Tudo bem?')
