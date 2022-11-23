dd245820ce8ac689f7fd42817f99b5cb73947c08
Marcelinhoj <m_seoy@msn.com>
2022-11-22 21:11:56 -0300

    1

--- cursos/14aula/17ex/sript.js
+++ cursos/14aula/17ex/sript.js
@@ -11,6 +11,7 @@ function tabuada() {
         while (c <= 10) {
             let item = document.createElement('option')
             item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
             tab.appendChild(item)
             c++
         }
