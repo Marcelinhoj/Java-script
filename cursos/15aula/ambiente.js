1e37e9778f39f9885861b1408b8dd9346f683674
Marcelinhoj <m_seoy@msn.com>
2022-11-23 21:19:07 -0300

    1

--- cursos/15aula/ambiente.js
+++ cursos/15aula/ambiente.js
@@ -4,3 +4,11 @@ num.sort()
 console.log(num)
 console.log(`O vetor tem ${num.length} posições`)
 console.log(`O Primeiro valor do vertor é ${num[0]}`)
+
+let pos = num.indexOf(8)
+if (pos == -1){
+    console.log('O Valor não foi encontrado')
+}else {
+   console.log(`O valor está na posição ${pos}`) 
+}
+ 
---
+++ cursos/15aula/vetornatela.js
@@ -0,0 +1,12 @@
+let valores = [8, 1, 7, 4, 2, 9]
+
+console.log(valores)
+/*
+
+for (let pos = 0; pos < valores.length; pos++) {
+    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+}*/
+
+for(let pos in valores){
+    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+}
